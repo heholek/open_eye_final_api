c1bda839fd392c5ae6da2accc6159a46e4e7d10c
Nitish Rastogi <nitish@micrasystems.com>
2020-03-08 05:08:27 +0530

    generated plane patient data for testing

--- backend/src/constroller/openEyeDataRecieved.js
+++ backend/src/constroller/openEyeDataRecieved.js
@@ -3,15 +3,29 @@ const getOpenEyeDataModle = require("../model/openEyeDataRecieved");
 const encryption = require("./encryption");
 const tokenValidation = require("./tokenValidation");
 const _ = require("lodash");
-var Base64 = require('js-base64').Base64;
+var Base64 = require("js-base64").Base64;
 
 mongoose.set("useUnifiedTopology", true);
+
+const getTestDataFromDB = (request, response) => {
+  getOpenEyeDataModle
+    .getDataFromDB(request.params.nhs_num)
+    .then(userData => {
+      response.json({ data: userData.received, status: "200" });
+    })
+    .catch(errorMessage => {
+      response.json({ message: "No NHS Number Found.", status: "404" });
+    });
+};
 
 const getDataFromDB = (request, response) => {
   getOpenEyeDataModle
     .getDataFromDB(request.params.nhs_num)
     .then(userData => {
-      response.json({ data: userData.received, status: "200" });
+      response.json({
+        data: generateCypherfromPatientData(userData.received),
+        status: "200"
+      });
     })
     .catch(errorMessage => {
       response.json({ message: "No NHS Number Found.", status: "404" });
@@ -31,8 +45,8 @@ const getAllDataFromDB = (request, response) => {
 
 const saveJson = (request, response) => {
   const receivedData = request.body.data;
-  const patientData = generatePatientDataFromCypher(receivedData)
-  if(patientData) {
+  const patientData = generatePatientDataFromCypher(receivedData);
+  if (patientData) {
     getOpenEyeDataModle.saveJson(patientData);
     response.json({ statu: "200", message: "successfully saved" });
   } else {
@@ -41,19 +55,32 @@ const saveJson = (request, response) => {
 };
 
 const generatePatientDataFromCypher = data => {
-  const decipherData = encryption.decipherThisString(data)
+  const decipherData = encryption.decipherThisString(data);
   try {
-    tokenValidation.verifyToke(decipherData)
+    tokenValidation.verifyToke(decipherData);
     const payLoadData = decipherData.split(".")[1];
     return JSON.parse(Base64.decode(payLoadData));
   } catch {
-    return false
+    return false;
   }
+};
 
-}
+const generateCypherfromPatientData = data => {
+  try {
+    const generatedToken = tokenValidation.generateToke(data);
+    const dataToBeEncrypted = {
+      token: generatedToken,
+      userData: data
+    };
+    return encryption.cipherThisString(JSON.stringify(dataToBeEncrypted));
+  } catch {
+    new Error();
+  }
+};
 
 module.exports = {
   getDataFromDB,
   getAllDataFromDB,
+  getTestDataFromDB,
   saveJson
 };
--- backend/src/routes/patient_data.js
+++ backend/src/routes/patient_data.js
@@ -10,5 +10,6 @@ const {
 routes.post("/openeyedata", saveJson);
 routes.get("/getdata", getAllDataFromDB);
 routes.get("/getdata/:nhs_num", getDataFromDB);
+routes.get("/get_test_data/:nhs_num", getTestDataFromDB);
 
 module.exports = routes;
