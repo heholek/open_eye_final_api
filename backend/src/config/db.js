7a9025a586cfe9d3165f15a17243b00257f37927
Nitish Rastogi <nitish@micrasystems.com>
2020-03-07 14:38:27 +0530

    db mysql

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,12 @@
 const Sequelize = require("sequelize");
 
-const formDB = new Sequelize("open_eye_db", "root", "", {
+const formDB = new Sequelize("open_eye_db", "openeye1", "Openeye1@#$21", {
   host: "localhost",
   dialect: "mysql"
   // timezone: "Asia/Kolkata"
 });
 
-const userResponse = new Sequelize("open_eye_answers_db", "root", "", {
+const userResponse = new Sequelize("open_eye_answers_db", "openeye2", "Openeye2@32!$", {
   host: "localhost",
   dialect: "mysql"
   // timezone: "Asia/Kolkata"
