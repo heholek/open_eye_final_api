1bf8796886d453622893fdfb052d996cd34e63f8
Nitish Rastogi <nitish@micrasystems.com>
2020-03-08 04:31:59 +0530

    patient recieved data encryption and token added

--- backend/src/constroller/encryption.js
+++ backend/src/constroller/encryption.js
@@ -1,45 +1,16 @@
-// var aesjs = require("aes-js");
+const Cryptr = require("cryptr");
+const { ENCRYP_KEY } = require("../config/credentials");
+const cryptr = new Cryptr(ENCRYP_KEY[0]);
 
-// console.log("inin")
+const cipherThisString = string => {
+  return cryptr.encrypt(string);
+};
 
-// // An example 128-bit key
-// var key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ];
+const decipherThisString = string => {
+  return cryptr.decrypt(string);
+};
 
-// // The initialization vector (must be 16 bytes)
-// var iv = [ 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,35, 36 ];
-
-// // Convert text to bytes (text must be a multiple of 16 bytes)
-// var text = '{"TextMustBe16Byte":"rgfghsdf"}';
-// var textBytes = aesjs.utils.utf8.toBytes(text);
-
-// var aesCbc = new aesjs.ModeOfOperation.cbc(key, iv);
-// var encryptedBytes = aesCbc.encrypt(textBytes);
-
-// // To print or store the binary data, you may convert it to hex
-// var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
-// console.log(encryptedHex);
-// // "104fb073f9a131f2cab49184bb864ca2"
-
-// // When ready to decrypt the hex string, convert it back to bytes
-// var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
-
-// // The cipher-block chaining mode of operation maintains internal
-// // state, so to decrypt a new instance must be instantiated.
-// var aesCbc = new aesjs.ModeOfOperation.cbc(key, iv);
-// var decryptedBytes = aesCbc.decrypt(encryptedBytes);
-
-// // Convert our bytes back into text
-// var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
-// console.log(decryptedText);
-// // "TextMustBe16Byte"
-
-// const generateCipher = (request, response) => {
-//     console.log(aesjs.utils.utf8.toBytes("aString"))
-// };
-
-// const deCipher = () => {};
-
-// module.exports = {
-//   generateCipher,
-//   deCipher
-// };
+module.exports = {
+  cipherThisString,
+  decipherThisString
+};
