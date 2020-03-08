41503ce0eef6f7b89740e20f2bd7e0f7de78d441
Nitish Rastogi <nitish@micrasystems.com>
2020-03-08 23:10:32 +0530

    removing comments

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,20 +8,11 @@ const PatientDataRoute = require("./routes/patient_data");
 const PatientCategoryRoute = require("./routes/patient_category");
 const Siteuser = require("./routes/site_user");
 
-const ValidateToken = require("./constroller/api_token_validation");
-const Encryption = require("./constroller/encryption");
-
 routes.use("/question", QuestionsRoute);
 routes.use("/site", SitesRoute);
 routes.use("/tile", TileRoute);
 routes.use("/patient_data", PatientDataRoute);
 routes.use("/patient_category", PatientCategoryRoute);
 routes.use("/user", Siteuser);
-
-// routes.use("/validate/token", ValidateToken.generateToken);
-// routes.use("/validate/test", ValidateToken.getDataFromToken);
-
-// routes.use("/cipher/generate", Encryption.generateCipher);
-// routes.use("/cipher/degenerate", Encryption.deCipher);
 
 module.exports = routes;
