6ad8444b81541480e0d72546f4aac2af1646c529
Nitish Rastogi <nitish@micrasystems.com>
2020-03-08 12:15:28 +0530

    removed mongodb cred

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 mongoose.set("useUnifiedTopology", true);
 
 const patientRecievedData_db = mongoose.createConnection(
-  "mongodb://root:Openeye@132#$@localhost:27017/openeyes",
+  "mongodb://localhost:27017/openeyes",
+  // "mongodb://root:Openeye@132#$@localhost:27017/openeyes",
   {
     useNewUrlParser: true
   }
 );
 
 const formQuestionsData_db = mongoose.createConnection(
-  "mongodb://root:Openeye@132#$@localhost:27017/open_eye",
+  "mongodb://localhost:27017/open_eye",
+  // "mongodb://root:Openeye@132#$@localhost:27017/open_eye",
   {
     useNewUrlParser: true
   }
