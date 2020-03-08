e08065d4ec9fcaf6059591e37b505d341983c467
Nitish Rastogi <nitish@micrasystems.com>
2020-03-08 05:41:51 +0530

    mongodb username and password added

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 mongoose.set("useUnifiedTopology", true);
 
 const patientRecievedData_db = mongoose.createConnection(
-  "mongodb://localhost:27017/openeyes",
+  "mongodb://root:Openeye@132#$@localhost:27017/openeyes",
   {
     useNewUrlParser: true
   }
 );
 
 const formQuestionsData_db = mongoose.createConnection(
-  "mongodb://localhost:27017/open_eye",
+  "mongodb://root:Openeye@132#$@localhost:27017/open_eye",
   {
     useNewUrlParser: true
   }
