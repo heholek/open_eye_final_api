49511488d1e41f95ea2f13538d93cedffc99878d
Nitish Rastogi <nitish@micrasystems.com>
2020-03-08 22:56:31 +0530

    test on server

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -5,11 +5,11 @@ const patientRecievedData_db = mongoose.createConnection(
   // "mongodb://localhost:27017/openeyes?authSource=admin",
   "mongodb://localhost:27017/openeyes",
   {
-    "auth": {
-      "authSource": "admin"
+    auth: {
+      authSource: "admin"
     },
-    "user": "root",
-    "pass": "Openeye@132#$"
+    user: "root",
+    pass: "Openeye@132#$"
   }
 );
 
@@ -17,11 +17,11 @@ const formQuestionsData_db = mongoose.createConnection(
   // "mongodb://localhost:27017/open_eye?authSource=admin",
   "mongodb://localhost:27017/open_eye",
   {
-    "auth": {
-      "authSource": "admin"
+    auth: {
+      authSource: "admin"
     },
-    "user": "root",
-    "pass": "Openeye@132#$"
+    user: "root",
+    pass: "Openeye@132#$"
   }
 );
 
