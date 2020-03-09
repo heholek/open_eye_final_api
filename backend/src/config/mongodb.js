8e5b0b678858a5f6df5d69e54956c83f30872db5
Nitish Rastogi <nitish@micrasystems.com>
2020-03-09 14:15:58 +0530

    test

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -4,25 +4,25 @@ mongoose.set("useUnifiedTopology", true);
 const patientRecievedData_db = mongoose.createConnection(
   // "mongodb://localhost:27017/openeyes?authSource=admin",
   "mongodb://localhost:27017/openeyes",
-  // {
-  //   auth: {
-  //     authSource: "admin"
-  //   },
-  //   user: "root",
-  //   pass: "Openeye@132#$"
-  // }
+  {
+    auth: {
+      authSource: "admin"
+    },
+    user: "root",
+    pass: "Openeye@132#$"
+  }
 );
 
 const formQuestionsData_db = mongoose.createConnection(
   // "mongodb://localhost:27017/open_eye?authSource=admin",
   "mongodb://localhost:27017/open_eye",
-  // {
-  //   auth: {
-  //     authSource: "admin"
-  //   },
-  //   user: "root",
-  //   pass: "Openeye@132#$"
-  // }
+  {
+    auth: {
+      authSource: "admin"
+    },
+    user: "root",
+    pass: "Openeye@132#$"
+  }
 );
 
 module.exports = {
